97a2d35df059e6766276c90315ba4470848d1eca
Umur ABALI <umurabali@gmail.com>
2018-07-13 15:00:33 +0300

    socket.io integration

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express');
+
 const app = express();
 const http = require('http').Server(app);
-const path = require('path');
+const io = require('socket.io')(http);
 
 app.use(express.static(__dirname));
 
@@ -9,8 +10,17 @@ app.get('/', (req, res) => {
   res.sendFile(`${__dirname}/index.html`);
 });
 
-http.listen(3000,() => {
-  console.log('listening on *:3000');
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+  socket.on('chat message', (msg) => {
+    console.log(`message: ${msg}`);
+    io.emit('chat message', msg);
+  });
 });
 
-
+http.listen(3000, () => {
+  console.log('listening on *:3000');
+});
